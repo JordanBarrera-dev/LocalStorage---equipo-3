cf514a8eed9487c029faeb485908fbe2985fbafb
Leisy Sánchez <leisysado@gmail.com>
2026-04-13 09:57:23 -0500

    Use set to avoid duplicated objects

--- apis/saveFavorite.js
+++ apis/saveFavorite.js
@@ -1,6 +1,6 @@
 function saveFavorite(pokemonInfo) {
     let list_favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    list_favorites.push(pokemonInfo);
-    localStorage.setItem("favorites", JSON.stringify(list_favorites));
-    //const favoritesDiv = document.querySelector(".list-favorites");
+    let favoritesSet = new Set(list_favorites);
+    favoritesSet.add(pokemonInfo);
+    localStorage.setItem("favorites", JSON.stringify([...favoritesSet]));
 }
